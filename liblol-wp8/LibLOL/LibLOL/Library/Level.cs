4f53d6ffee5ce99ad28e14893a7a9f6408ec632c
mlw214 <miller.wilt@gmail.com>
2014-03-25 23:33:01 -0400

    Created initial MonoGame project. Included Farseer physics.
    Created a WP8 MonoGame project. Imported Farseer physics files into
    project, and cleared up some errors dealing with ambiguous references
    to classes. Started Level class.

---
+++ liblol-wp8/LibLOL/LibLOL/Library/Level.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Media;
+
+namespace LibLOL.Library
+{
+    public class Level : DrawableGameComponent
+    {
+        public Level(Game game) : base(game)
+        {
+
+        }
+    }
+}
