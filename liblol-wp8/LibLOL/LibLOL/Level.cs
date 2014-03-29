dd1e53fb2b63fa20f07461b531fdcf6c1232ea29
mlw214 <miller.wilt@gmail.com>
2014-03-29 17:11:35 -0400

    Created VS solution for Monogame development. Started converting LibLOL code
    Created Monogame project (v3.2 build 347) and started converting LibLOL
    code. Farseer Physics has been included and fixed up a bit to work
    correctly.

---
+++ liblol-wp8/LibLOL/LibLOL/Level.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Media;
+using FarseerPhysics.Dynamics;
+
+namespace LibLOL
+{
+    public class Level : DrawableGameComponent
+    {
+        private Song music;
+
+        private bool musicIsPlaying;
+
+        // Score score = new Score();
+
+        // Tilt tilt = new Tilt();
+
+
+        World world;
+
+        public Level(Game game) : base(game)
+        {
+
+        }
+    }
+}
---
+++ liblol-wp8/LibLOL/LibLOL/LolConfiguration.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace LibLOL
+{
+    public interface LolConfiguration
+    {
+        // Like Java, interface methods are automatically public
+        int GetScreenWidth();
+
+        int GetScreenHeight();
+
+        int GetNumLevels();
+
+        int GetNumHelpScenes();
+
+        bool GetVibration();
+
+        bool GetUnlockMode();
+
+        bool ShowDebugBoxes();
+
+        String GetStorageKey();
+
+        String GetDefaultFontFace();
+
+        int GetDefaultFontSize();
+
+        int GetDefaultFontRed();
+
+        int GetDefaultFontGreen();
+
+        int GetDefaultFontBlue();
+
+        String GetDefaultWinText();
+
+        String GetDefaultLoseText();
+
+        String GetGameTitle();
+    }
+}
